326d6a84c3b52d528829cc7ce66ee41c800bad89
jacks <jacks@192.168.1.34>
2024-01-18 11:32:46 +0530

    First commit...

---
+++ src/org/demo/github/Test1.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.demo.github;
+
+/**
+ * 
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 =10;
+	     System.out.println("Stored value is "+num1);
+	     boolean flag = true;
+	     System.out.println("Boolean value stored is "+flag);
+
+	}
+
+}
