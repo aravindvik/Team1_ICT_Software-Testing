4145efc15222542ac12233cf56c6012583ec0572
jagat <jagat@Diya>
2024-01-19 22:15:52 +0530

    Comiting the Test 3 to Branch1

---
+++ src/org/demo/github/Test3.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.demo.github;
+
+/**
+ * 
+ */
+public class Test3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 =5;
+	     System.out.println("The Stored value is "+num1);
+	     boolean flag = true;
+	     System.out.println("The Boolean value stored is "+flag);
+
+	}
+
+}
